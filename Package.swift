a0cc0f1829be1ff8b3c3cfa7a9f4b0c620946a54
gi8 <giottolino@gmail.com>
2026-08-10 21:31:49 +0200

    feat: bump easybar-kit

--- Package.swift
+++ Package.swift
@@ -2,44 +2,58 @@
 
 import PackageDescription
 
+let strictConcurrencySettings: [SwiftSetting] = [
+  .enableUpcomingFeature("StrictConcurrency")
+]
+
 let package = Package(
   name: "EasyBar",
   platforms: [
     .macOS(.v14)
   ],
   products: [
-    .executable(name: "EasyBar", targets: ["EasyBarApp"])
+    .executable(
+      name: "EasyBar",
+      targets: ["EasyBarApp"]
+    )
   ],
   dependencies: [
-    // .package(path: "../easybar-kit")
     .package(
       url: "https://github.com/easybar-app/easybar-kit",
-      from: "0.54.0",
+      from: "0.53.2"
     )
   ],
   targets: [
     .executableTarget(
       name: "EasyBarApp",
       dependencies: [
-        .product(name: "EasyBarKit", package: "easybar-kit"),
-        .product(name: "EasyBarShared", package: "easybar-kit"),
+        .product(
+          name: "EasyBarKit",
+          package: "easybar-kit"
+        ),
+        .product(
+          name: "EasyBarShared",
+          package: "easybar-kit"
+        ),
       ],
       path: "Sources/EasyBarApp",
-      swiftSettings: [
-        .enableUpcomingFeature("StrictConcurrency")
-      ]
+      swiftSettings: strictConcurrencySettings
     ),
     .testTarget(
       name: "EasyBarAppTests",
       dependencies: [
         "EasyBarApp",
-        .product(name: "EasyBarKit", package: "easybar-kit"),
-        .product(name: "EasyBarShared", package: "easybar-kit"),
+        .product(
+          name: "EasyBarKit",
+          package: "easybar-kit"
+        ),
+        .product(
+          name: "EasyBarShared",
+          package: "easybar-kit"
+        ),
       ],
       path: "Tests/EasyBarAppTests",
-      swiftSettings: [
-        .enableUpcomingFeature("StrictConcurrency")
-      ]
+      swiftSettings: strictConcurrencySettings
     ),
   ]
 )
